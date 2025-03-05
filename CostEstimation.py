5c7d63c926abd71570d0cb0f3a92a853259217fb
Conall98 <conalldepaor@gmail.com>
2025-03-05 10:44:07 +0100

    first uncertainty results

--- CostEstimation.py
+++ CostEstimation.py
@@ -13,7 +13,7 @@ Created on Mon Mar  3 12:26:44 2025
 #Landed Payload [kg]
 #Returned Payload [kg]
 
-class MA:
+class MA_FOM:
     def __init__(self, 
                  SAC, 
                  MMC,
@@ -30,4 +30,102 @@ class MA:
         self.LP = LP
         self.RP = RP
     
-    
+#%% MA1
+#MMC is launch cost +ten percent
+flights = 20
+mpd = 2000
+mpr = 2000
+total_mpd = flights*mpd
+total_mpr = flights*mpr
+SAC = 420.1*1e6
+OPC=778.6*1e6
+MMC = OPC/flights
+LCC = SAC + OPC
+LP = mpd
+RP = mpr
+DT = 7
+SPDC = LCC/total_mpd
+SPRC = LCC/total_mpr
+MA1 = MA_FOM(SAC, MMC, SPDC, SPRC, DT, LP, RP)
+
+
+#%% MA2
+#MMC is launch cost +ten percent
+flights = 20
+mpd = 2000
+mpr = 2000
+DT = 7
+SAC = 420.1*1e6
+OPC=778.6*1e6
+
+total_mpd = flights*mpd
+total_mpr = flights*mpr
+MMC = OPC/flights
+LCC = SAC + OPC
+LP = mpd
+RP = mpr
+
+SPDC = LCC/total_mpd
+SPRC = LCC/total_mpr
+MA1 = MA_FOM(SAC, MMC, SPDC, SPRC, DT, LP, RP)
+
+#%% MA3
+#MMC is launch cost +ten percent
+flights = 20
+mpd = 2000
+mpr = 2000
+DT = 7
+SAC = 420.1*1e6
+OPC=778.6*1e6
+
+total_mpd = flights*mpd
+total_mpr = flights*mpr
+MMC = OPC/flights
+LCC = SAC + OPC
+LP = mpd
+RP = mpr
+
+SPDC = LCC/total_mpd
+SPRC = LCC/total_mpr
+MA1 = MA_FOM(SAC, MMC, SPDC, SPRC, DT, LP, RP)
+
+#%% MA4
+#MMC is launch cost +ten percent
+flights = 20
+mpd = 2000
+mpr = 2000
+DT = 7
+SAC = 420.1*1e6
+OPC=778.6*1e6
+
+total_mpd = flights*mpd
+total_mpr = flights*mpr
+MMC = OPC/flights
+LCC = SAC + OPC
+LP = mpd
+RP = mpr
+
+SPDC = LCC/total_mpd
+SPRC = LCC/total_mpr
+MA1 = MA_FOM(SAC, MMC, SPDC, SPRC, DT, LP, RP)
+
+#%% MA5
+#MMC is launch cost +ten percent
+flights = 20
+mpd = 2000
+mpr = 2000
+DT = 7
+SAC = 420.1*1e6
+OPC=778.6*1e6
+
+total_mpd = flights*mpd
+total_mpr = flights*mpr
+MMC = OPC/flights
+LCC = SAC + OPC
+LP = mpd
+RP = mpr
+
+SPDC = LCC/total_mpd
+SPRC = LCC/total_mpr
+MA1 = MA_FOM(SAC, MMC, SPDC, SPRC, DT, LP, RP)
+
--- uncertainty testing.py
+++ uncertainty testing.py
@@ -20,28 +20,109 @@ import matplotlib.pyplot as plt
 # import pandas as pd
 #%% MA-5 2t
 # mp = np.random.normal(3000, 20, 1000)
-mp=3000
+mp = 2000
+mp_sd = 100 #5%
+
 L_dv = 2500
+L_dv_sd = 125 #5%
+
 L_Isp = 320
+L_Isp_sd = 16 #5%
+
+
+
+i = np.random.normal(mp, mp_sd, 1000)
+j = np.random.normal(L_dv, L_dv_sd, 1000)
+k = np.random.normal(L_Isp, L_Isp_sd, 1000)
+
+
+#%% Gaussian Payload
 bucket = []
-i = np.random.normal(3000, 200, 1000)
-j = L_dv
-k = L_Isp
+bucket2 = []
+bucket3 = []
 for A in i:
 # for j in L_dv:
 #     for k in L_Isp:
-        L1, Test = LANR.routine_1(A, j, k)
+        L1, Test = LANR.routine_1(A, L_dv, L_Isp)
         LV1 = LAUR.routine_1(L1)
         bucket.append(L1.md)
+        bucket2.append(L1.mprop)
+        bucket3.append(L1.mt)
+#%%
+bucket = []
+bucket2 = []
+bucket3 = []
+for A in j:
+# for j in L_dv:
+#     for k in L_Isp:
+        L1, Test = LANR.routine_1(mp, A, L_Isp)
+        LV1 = LAUR.routine_1(L1)
+        bucket.append(L1.md)
+        bucket2.append(L1.mprop)
+        bucket3.append(L1.mt)
+        
+#%%
+bucket = []
+bucket2 = []
+bucket3 = []
+for A in k:
+# for j in L_dv:
+#     for k in L_Isp:
+        L1, Test = LANR.routine_1(mp, L_dv, A)
+        LV1 = LAUR.routine_1(L1)
+        bucket.append(L1.md)
+        bucket2.append(L1.mprop)
+        bucket3.append(L1.mt)
 
 #%% Histagrams
 
-plt.figure()
-mean = np.mean(bucket)
-plt.hist(bucket, 100)
-plt.axvline(mean, color = "black", linestyle = "--")
-plt.xlabel("dry mass")
+# plt.figure()
+# mean = np.mean(bucket)
+# plt.hist(bucket, 100)
+# plt.axvline(mean, color = "black", linestyle = "--")
+# plt.xlabel("dry mass")
 
+# plt.figure()
+# plt.hist(i, 100)
+# plt.xlabel("mp")
+#%% ijk QUAD PLOTS GAUSSES
+fig, axs = plt.subplots(2,2)
+fig.suptitle("Uncertainty Propagation")
+
+axs[0,0].hist(k, 100)
+mean=np.mean(k)
+axs[0,0].axvline(mean, color = "black", linestyle = "--")
+axs[0,0].set_title("Input Uncertainty (Isp) sd = 5%")
+axs[0,0].set_xlabel("Isp")
+
+axs[0,1].hist(bucket, 100)
+mean=np.mean(bucket)
+axs[0,1].axvline(mean, color = "black", linestyle = "--")
+axs[0,1].set_title("Output Uncertainty (md)")
+axs[0,1].set_xlabel("dry mass")
+
+axs[1,0].hist(bucket2, 100)
+mean=np.mean(bucket2)
+axs[1,0].axvline(mean, color = "black", linestyle = "--")
+axs[1,0].set_title("Output Uncertainty (mprop)")
+axs[1,0].set_xlabel("propellant mass")
+
+axs[1,1].hist(bucket3, 100)
+mean=np.mean(bucket3)
+axs[1,1].axvline(mean, color = "black", linestyle = "--")
+axs[1,1].set_title("Output Uncertainty (mt)")
+axs[1,1].set_xlabel("Total Mass")
+
+#%%
 plt.figure()
-plt.hist(i, 100)
-plt.xlabel("mp")
+plt.title("dv and the params when Isp is gaussian")
+plt.scatter(j, bucket, label = "dry mass")
+plt.scatter(j, bucket2, label = "propellant mass")
+plt.scatter(j, bucket3, label = "total mass")
+plt.legend()
+
+#%%
+plt.figure()
+plt.title("mp and dv")
+plt.scatter(i, bucket, label = "dry mass")
+plt.legend()
